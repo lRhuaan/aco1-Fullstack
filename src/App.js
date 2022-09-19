6d40870c9be5ce0be0dfb02f583a88f395166491
lRhuaan <r.espigares_@hotmail.com>
2022-09-19 20:30:46 -0300

    Formulario

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react'
+import {useState} from 'react';
 import './App.css';
 
-function App() {
+function Formulario() {
+  const [formValues, setFormValues] = useState({});
+
+  const handleInputChange = (e) =>{
+    const {name, value} = e.target
+    setFormValues({...formValues, [name]: value});
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData);
+
+
+    console.log('*** handleSubmit', data);
+  };
+
+  console.log('*** formValues', formValues);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <h1>Nome:</h1>
+          <input type="text" name="name" placeholder="name" onChange={handleInputChange} value={formValues.name || ''}/>
+          <h1>Email:</h1>
+          <input type="text" name="email" placeholder="email" onChange={handleInputChange} value={formValues.email || ''}/>
+
+          <h1>Sua linguagem favorita:</h1>
+          <select name="language" onChange={handleInputChange} value={formValues.language || ''}>
+            <option value="javascript">javascript</option>
+            <option value="Python">Python</option>
+            <option value="Ruby">Ruby</option> 
+          </select>
+
+          <h1>Sua bebida favorita</h1>
+          <div className='radios'>
+            <label>
+              <input type='radio' value='cafe' name='drink' onChange={handleInputChange} checked={formValues.drink === 'cafe'}/> Café
+            </label>
+            <label>
+              <input type='radio' value='cha' name='drink' onChange={handleInputChange} checked={formValues.drink === 'cha'}/> Chá
+            </label>
+          </div>
+
+          <h1>Deixe sua mensagem</h1>
+          <textarea name='bio' onChange={handleInputChange} value={formValues.bio || ''}></textarea>
+
+          <button type="submit">Enviar</button>
+        </form>
+      </div>
+      
   );
 }
 
-export default App;
+export default Formulario;
